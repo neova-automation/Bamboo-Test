a5d6d6326458d7fb9cce26954ae56a0e1e6faadf
neova-automation <neova.automation@gmail.com>
2015-12-31 10:56:48 +0530

    IE DRiver

--- SDX_Release_1/src/homepage/NewTest1.java
+++ SDX_Release_1/src/homepage/NewTest1.java
@@ -1,16 +1,22 @@
 package homepage;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.ie.*;
 
 public class NewTest1 {
   @Test
   public void f() {	  
 	  
 	  	System.out.println("In Process Firefox Script");
-	  	WebDriver driver = new FirefoxDriver(); 
+	  	
+	  	File file = new File("IEDriverServer.exe");
+	  	System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+	  	WebDriver driver = new InternetExplorerDriver();
 	  	System.out.println("Driver Initiazed");
 		//driver.manage().window().maximize(); 
 		driver.get("https://www.sdxcentral.com/"); // Navigate to SDXCentral 
